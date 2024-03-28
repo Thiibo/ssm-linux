792fea58fb6c2f09234836499f5a41dde98948e4
Thiibo <thibodev@protonmail.com>
2024-03-28 16:52:53 +0100

    Add watch scripts to startup script

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -3,6 +3,9 @@
 # Do stuff
 echo [$(date)] [$(whoami)@$(hostname)] Server started. >> ~/server.log
 
+./watch-shadow-file.sh &
+./watch-ssh.sh &
+
 # Send mail
 recipient="incoming@localhost"
 subject="Server startup script performed for $(hostname)"
